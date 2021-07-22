7e997aa222dc3cfea94f27fc1bb56923c431ca10
Daniel Allen <daniel.allen@centrapay.com>
2021-07-22 10:20:21 -0600

    Add github pages deploy script

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hey There, looks like it worked"/>
 </template>
 
 <script>
